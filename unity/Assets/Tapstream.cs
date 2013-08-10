a6f09628f5ba33bcec7c7becbf33a0685afee205
Eric Roy <ericsroy@gmail.com>
2013-08-10 14:58:00 -0700

    Unity

---
+++ unity/Assets/Tapstream.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tapstream : MonoBehaviour {
+
+	[DllImport ("ASimplePlugin")]
+	private static extern int PrintANumber();
+	
+	[DllImport ("ASimplePlugin")]
+	private static extern IntPtr PrintHello();
+	
+	[DllImport ("ASimplePlugin")]
+	private static extern int AddTwoIntegers(int i1,int i2);
+
+	[DllImport ("ASimplePlugin")]
+	private static extern float AddTwoFloats(float f1,float f2);	
+	
+	void Start () {
+		Debug.Log(PrintANumber());
+		Debug.Log(Marshal.PtrToStringAuto (PrintHello()));
+		Debug.Log(AddTwoIntegers(2,2));
+		Debug.Log(AddTwoFloats(2.5F,4F));
+	}
+
+}
---
+++ unity/TapstreamObjcInterface.m
@@ -0,0 +1,54 @@
+#include "Tapstream/Tapstream.h"
+
+static TSConfig *conf = [TSConfig configWithDefaults];
+
+void Config_Set(const char *key, const char *value)
+{
+}
+void Config_Set(const char *key, bool value)
+{
+}
+void Config_Set(const char *key, int value)
+{
+}
+void Config_Set(const char *key, unsigned int value)
+{
+}
+void Config_Set(const char *key, double value)
+{
+}
+
+
+void *Event_New(const char *name, bool oneTimeOnly)
+{
+
+}
+void Event_AddPair(void *event, const char *key, const char *value)
+{
+
+}
+void Event_AddPair(void *event, const char *key, bool value)
+{
+
+}
+void Event_AddPair(void *event, const char *key, int value)
+{
+
+}
+void Event_AddPair(void *event, const char *key, unsigned int value)
+{
+
+}
+void Event_AddPair(void *event, const char *key, double value)
+{
+
+}
+
+void Tapstream_Create(const char *accountName, const char *developerSecret, void *config)
+{
+
+}
+void Tapstream_FireEvent(void *event)
+{
+
+}
