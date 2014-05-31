6046910d6596612f20636ff5d068146d8da455a5
eroy <ericsroy@gmail.com>
2014-05-31 14:56:00 -0700

    Update pavement file to include u2u files

--- pavement.py
+++ pavement.py
@@ -241,6 +241,9 @@ def package_objc():
 		for file_type in ('.h', '.m'):
 			sh('cp ./objc/Tapstream/*%s ./builds/%s/Tapstream/' % (file_type, sdk))
 			sh('cp ./objc/Core/*%s ./builds/%s/Tapstream/' % (file_type, sdk))
+		if sdk == 'ios':
+			path('builds/%s/UserToUser' % sdk).makedirs()
+			sh('cp -r ./objc/U2U/* ./builds/%s/UserToUser/' % sdk)
 		path('builds/tapstream-%s-%s.zip' % (VERSION, sdk)).remove()
 		with pushd('builds/%s' % sdk):
 			sh('zip -r ../tapstream-%s-%s.zip Tapstream' % (VERSION, sdk))
@@ -249,6 +252,7 @@ def package_objc():
 		path('builds/%s-whitelabel' % sdk).rmtree()
 		path('builds/%s-whitelabel' % sdk).mkdir()
 		sh('cp -r ./builds/%s/Tapstream ./builds/%s-whitelabel/' % (sdk, sdk))
+		sh('cp -r ./builds/%s/UserToUser ./builds/%s-whitelabel/' % (sdk, sdk))
 		with pushd('./builds/%s-whitelabel' % sdk):
 			sh('mv Tapstream ConversionTracker')
 			sh('mv ConversionTracker/TSTapstream.h ConversionTracker/ConversionTracker.h')
