17aa4f7225bd55a404ae77b76728bdaf0d4b983a
eroy <ericsroy@gmail.com>
2014-05-31 14:27:50 -0700

    More testing functionality in test app

--- examples/iOS/UserToUser/UserToUser/ViewController.h
+++ examples/iOS/UserToUser/UserToUser/ViewController.h
@@ -8,13 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
 
 @property(strong) IBOutlet UITextField *insertionPointText;
+@property(strong) IBOutlet UITextView *rewardsList;
+@property(strong) IBOutlet UITextField *rewardSku;
+
+@property(strong) NSMutableDictionary *rewards;
 
 - (IBAction)onTestInsertionPoint:(id)sender;
 - (IBAction)onMakeInstallTimeAgesAgo:(id)sender;
 - (IBAction)onEraseLastShownTimes:(id)sender;
 - (IBAction)onKill:(id)sender;
+- (IBAction)onRefreshRewards:(id)sender;
+- (IBAction)onConsumeReward:(id)sender;
 
 @end
--- examples/iOS/UserToUser/UserToUser/ViewController.m
+++ examples/iOS/UserToUser/UserToUser/ViewController.m
@@ -16,13 +16,13 @@
 
 @implementation ViewController
 
-@synthesize insertionPointText;
+@synthesize insertionPointText, rewardsList, rewardSku, rewards;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
+    self.rewards = [NSMutableDictionary dictionary];
     self.view.layer.backgroundColor = [UIColor colorWithRed:0.6 green:0.8 blue:1.0 alpha:1.0].CGColor;
 }
 
@@ -59,10 +59,52 @@
     exit(0);
 }
 
+- (IBAction)onRefreshRewards:(id)sender
+{
+    [(UIButton *)sender setEnabled:NO];
+    [rewardsList setText:@""];
+    self.rewards = [NSMutableDictionary dictionary];
+    TSUserToUserController *u2u = [TSTapstream userToUserController];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSArray *results = [u2u availableRewards];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSString *rewardSkus = @"";
+            [results enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                [rewardSkus stringByAppendingString:[NSString stringWithFormat:@"%lu, %@\n", (unsigned long)((TSReward *)obj).ident, ((TSReward *)obj).name]];
+                [self.rewards setObject:obj forKey:[[NSNumber numberWithInteger:((TSReward *)obj).ident] stringValue]];
+            }];
+            [rewardsList setText:rewardSkus];
+            [(UIButton *)sender setEnabled:YES];
+        });
+    });
+}
+
+- (IBAction)onConsumeReward:(id)sender
+{
+    TSReward *reward = [self.rewards objectForKey:self.rewardSku.text];
+    if(reward) {
+        TSUserToUserController *u2u = [TSTapstream userToUserController];
+        [u2u consumeReward:reward];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid reward id"
+                                                        message:@"Enter the numeric id of the reward to consume"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
