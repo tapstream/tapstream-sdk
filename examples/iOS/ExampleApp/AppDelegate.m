055181c3c7764e9c03f7414a85520c68b91fb928
Benjamin Fox <fox.benjamin@gmail.com>
2015-02-03 15:52:48 -0800

    Fixing a problem with the iOS getConversionData example

--- examples/iOS/ExampleApp/AppDelegate.m
+++ examples/iOS/ExampleApp/AppDelegate.m
@@ -26,33 +26,41 @@
     [config.globalEventParams setValue:@25.4 forKey:@"degrees"];
     
     [TSTapstream createWithAccountName:@"sdktest" developerSecret:@"YGP2pezGTI6ec48uti4o1w" config:config];
+
     
     TSTapstream *tracker = [TSTapstream instance];
-//    [tracker getConversionData:^(NSData *jsonInfo) {
-//        if(jsonInfo == nil)
-//        {
-//            // No conversion data available
-//            NSLog(@"No conversion data");
-//        }
-//        else
-//        {
-//            NSLog(@"Conversion data: %@", [[NSString alloc] initWithData:jsonInfo encoding:NSUTF8StringEncoding]);
-//            NSError *error;
-//            NSArray *json = [NSJSONSerialization JSONObjectWithData:jsonInfo options:kNilOptions error:&error];
-//            if(json && !error)
-//            {
-//                // Read some data from this json object, and modify your application's behaviour accordingly
-//                // ...
-//            }
-//        }
-//    }];
-    
+    [tracker getConversionData:^(NSData *jsonInfo) {
+        if(jsonInfo == nil)
+        {
+            // No conversion data available. This might be because Tapstream
+            // took longer than 10 seconds to respond, or because the Tapstream
+            // API server returned a 4xx response.
+        }
+        else
+        {
+            NSError *error      = nil;
+            NSDictionary *jsonDict  = [NSJSONSerialization JSONObjectWithData:jsonInfo
+                                                                      options:kNilOptions
+                                                                        error:&error];
+            
+            
+            if(jsonDict && !error)
+            {
+                NSArray *hits = [jsonDict objectForKey:@"hits"];
+                NSArray *events = [jsonDict objectForKey:@"events"];
+                
+                NSLog(@"Hits: %@", hits);
+                NSLog(@"Events: %@", events);
+                
+            }
+        }
+    }];
     
     TSEvent *e = [TSEvent eventWithName:@"test-event" oneTimeOnly:NO];
     [e addValue:@"John Doe" forKey:@"player"];
     [e addValue:@10.1 forKey:@"degrees"];
     [e addValue:@5 forKey:@"score"];
-    [tracker fireEvent:e];
+    [[TSTapstream instance] fireEvent:e];
 
     [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
     
