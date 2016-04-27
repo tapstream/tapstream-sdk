08bbe6755a1bb317e6ed685f1a1bf79b74dda75f
Nick Sitarz <nsitarz@gmail.com>
2016-04-27 16:19:37 -0700

    Android: improve javadoc coverage

--- java/tapstream-core/src/main/java/com/tapstream/sdk/ApiResponse.java
+++ java/tapstream-core/src/main/java/com/tapstream/sdk/ApiResponse.java
@@ -4,5 +4,10 @@ package com.tapstream.sdk;
 import com.tapstream.sdk.http.HttpResponse;
 
 public interface ApiResponse {
+
+    /**
+     * Returns the {@link HttpResponse} this instance was created from.
+     * @return the associated http response.
+     */
     HttpResponse getHttpResponse();
 }
