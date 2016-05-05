82021394f24dbd2f3a7778be892a86516a5ef17a
Nick Sitarz <nsitarz@gmail.com>
2016-05-05 10:34:19 -0700

    remove android sdk

--- java/Core/src/main/java/com/tapstream/sdk/Delegate.java
+++
@@ -1,8 +0,0 @@
-package com.tapstream.sdk;
-
-interface Delegate {
-	public void init(Core core);
-	public int getDelay();
-	public void setDelay(int delay);
-	public boolean isRetryAllowed();
-}
---
+++ java/README.markdown
@@ -0,0 +1,3 @@
+# Moved to tapstream/tapstream-sdk-android
+
+The Tapstream Android SDK is now available [here](https://github.com/tapstream/tapstream-sdk-android)
